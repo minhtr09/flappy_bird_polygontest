4689c9ff197f286e0fdaf17c4dcadc83db8336de
tringuyenskymavis <tri.minh.nguyen@skymavis.com>
2024-05-19 10:10:14 +0700

    chore: forge init

--- Contracts/hardhat.config.ts
+++ Contracts/hardhat.config.ts
@@ -3,21 +3,43 @@ import "@nomicfoundation/hardhat-toolbox";
 require("dotenv").config();
 import "@nomicfoundation/hardhat-verify";
 
-const { API_URL, PRIVATE_KEY, POLYGONSCAN_KEY } = process.env;
+const { API_URL, PRIVATE_KEY, SAIGON_API_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.20",
-  defaultNetwork: "mumbai",
+  namedAccounts: {
+    deployer:
+      "privatekey://0x2504f2e738bf5bf71ea9ddbec3e4c895dfe3b7d056f9e5f12055941fea74c092",
+  },
+  sourcify: {
+    enabled: true,
+  },
+  defaultNetwork: "saigon",
   networks: {
     hardhat: {},
     mumbai: {
       url: API_URL,
       accounts: [PRIVATE_KEY],
     },
+    saigon: {
+      // chainId: 2021,
+      url: "https://saigon-testnet.roninchain.com/rpc",
+      accounts: [PRIVATE_KEY],
+    },
   },
   etherscan: {
     apiKey: {
-      polygonMumbai: POLYGONSCAN_KEY,
+      saigon: SAIGON_API_KEY,
     },
+    customChains: [
+      {
+        network: "saigon",
+        chainId: 2021,
+        urls: {
+          apiURL: "https://sourcify.roninchain.com/server/",
+        },
+      },
+    ],
   },
+ 
 };
--- Contracts/scripts/deploy.ts
+++ Contracts/scripts/deploy.ts
@@ -1,11 +1,11 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  // const Floppy = await ethers.getContractFactory("Floppy");
-  // const floppy = await Floppy.deploy(
-  //   "0xf9577C9df5141B8793df5B869E0548dD8fBe3Ce9"
-  // );
-  // console.log("Floppy address: ", floppy.target);
+  const Floppy = await ethers.getContractFactory("Floppy");
+  const floppy = await Floppy.deploy(
+    "0x74fe5f04ccbe9c0dbcd0039cfb8d7212b2e6e452"
+  );
+  console.log("Floppy address: ", floppy.target);
 
   // const USDT = await ethers.getContractFactory("USDT");
   // const usdt = await USDT.deploy("0xf9577C9df5141B8793df5B869E0548dD8fBe3Ce9");
@@ -15,15 +15,16 @@ async function main() {
   // const bird = await Bird.deploy("0xf9577C9df5141B8793df5B869E0548dD8fBe3Ce9");
   // console.log("Bird NFT address: ", bird.target);
 
-  const FLPCrowdSale = await ethers.getContractFactory("FLPCrowdSale");
-  const flpcrowndsale = await FLPCrowdSale.deploy(
-    "0xf9577C9df5141B8793df5B869E0548dD8fBe3Ce9",
-    10000,
-    5000,
-    "0x2A744DbD8F36632dEeC27E899B8D80a71fA9545C",
-    "0xCCec906001af3cF82ceb78E3f078eD04C3274C45"
-  );
-  console.log("Bird NFT address: ", flpcrowndsale.target);
+
+  // const FLPCrowdSale = await ethers.getContractFactory("FLPCrowdSale");
+  // const flpcrowndsale = await FLPCrowdSale.deploy(
+  //   "0xf9577C9df5141B8793df5B869E0548dD8fBe3Ce9",
+  //   10000,
+  //   5000,
+  //   "0x2A744DbD8F36632dEeC27E899B8D80a71fA9545C",`nosc
+  //   "0xCCec906001af3cF82ceb78E3f078eD04C3274C45"
+  // );
+  // console.log("Bird NFT address: ", flpcrowndsale.target);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
